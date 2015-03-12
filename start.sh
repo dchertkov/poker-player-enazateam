a47c467cf937092424dd6944d31617946a61ceca
Bence Kiglics <bence.kiglics@prezi.com>
2015-03-12 06:10:05 +0100

    Removed start/stop scripts

--- start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd $DIR
-
-php5 -S 'localhost:8000' & 
-
-echo $! > php_server.pid
--- stop.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd $DIR
-
-kill `cat php_server.pid`
-rm php_server.pid
