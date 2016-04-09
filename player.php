bf95ec2de5e622720209b69761c434ad216e2e41
Dmitriy Chertkov <dmitriychertkov@gmail.com>
2016-04-09 13:02:22 +0500

    v2.1

--- player.php
+++ player.php
@@ -5,26 +5,22 @@ require_once('functions/deciding.php');
 
 class Player
 {
-    const VERSION = "2.0";
+    const VERSION = "2.1";
 
     public function betRequest($game_state)
     {
     	foreach ($game_state['players'] as $p) {
     		if ($p['name'] == 'EnazaTeam') {
-    			break;
+				$card1 = $p['hole_cards'][0];
+		    	$card2 = $p['hole_cards'][1];
+
+		    	$result = fCheckProbability($card1['rank'], $card2['rank'], $card1['suit'] == $card2['suit']);
+
+		    	return $result > 60 ? 1000000 : 0;
     		}
     	}
 
-    	if (isset($p)) {
-	    	$card1 = $p['hole_cards'][0];
-	    	$card2 = $p['hole_cards'][1];
-
-	    	$result = fCheckProbability($card1['rank'], $card2['rank'], $card1['suit'] == $card2['suit']);
-
-	    	return $result > 60 ? 1000000 : 0;
-	    } else {
-	    	return 1000000;
-	    }
+    	return 1000000;
     }
 
     public function showdown($game_state)
