e3623796f02dc060f1253e0dc5dda67d6f26f93c
Dmitriy Chertkov <dmitriychertkov@gmail.com>
2016-04-09 12:56:12 +0500

    v2.0

---
+++ functions/deciding.php
@@ -0,0 +1,54 @@
+<?php
+require_once(dirname(__FILE__) . "/fCheckProbability.php");
+
+// общий фактор агрессии, 1 - без корректировок
+$aggressionFactor = 1;
+
+// фактор увеличения важности позиции, за одну позицию
+$positionFactor = 1.01;
+
+// фактор страха лимперов, уменьшает процент успешного олина за каждого лимпера
+$limpersFactor = 0.99;
+
+// фактор страха рейзеров, уменьшает процент успешного олина за каждого рейзера
+$raisersFactor = 0.95;
+
+// значение параметра false = игнорировать
+function deciding($card1, $card2, $isSuited, $position = false, $limpersCount = false, $raisersCount = false, $allInCount = false) {
+	global $aggressionFactor, $positionFactor, $limpersFactor, $raisersFactor;
+
+	// количество противников, если есть олинщики, считаем от их количества, если нет, считаем для одного
+	$enemyCount = $allInCount ? $allInCount : 1;
+
+	// считаем базовую вероятность против количества 100% - противников на флопе
+	$baseProbability = fCheckProbability($card1, $card2, $isSuited, $enemyCount);
+
+	echo "Базовая вероятность для $enemyCount соперников $baseProbability \n";
+
+	// увеличиваем вероятность из-за позиции, если есть
+	if ($position) {
+		for ($i = 1; $i <= $position; $i++) {
+			$baseProbability = $baseProbability * $positionFactor;
+		}
+	}
+
+	// уменьшаем вероятность из-за лимперов, если есть
+	if ($limpersCount) {
+		for ($i = 1; $i <= $limpersCount; $i++) {
+			$baseProbability = $baseProbability * $limpersFactor;
+		}
+	}
+
+	// уменьшаем вероятность из-за рейзеров, если есть
+	if ($raisersCount) {
+		for ($i = 1; $i <= $raisersCount; $i++) {
+			$baseProbability = $baseProbability * $raisersFactor;
+		}
+	}
+
+	echo "Вероятность с учетом других факторов $baseProbability \n";
+
+	$baseProbability = $baseProbability * $aggressionFactor;
+}
+
+// deciding("A", "K", false, 4, 5, 1);
--- player.php
+++ player.php
@@ -1,12 +1,30 @@
 <?php
 
+require_once('functions/fCheckProbability.php');
+require_once('functions/deciding.php');
+
 class Player
 {
-    const VERSION = "Default PHP folding player";
+    const VERSION = "2.0";
 
     public function betRequest($game_state)
     {
-        return 1000000;
+    	foreach ($game_state['players'] as $p) {
+    		if ($p['name'] == 'EnazaTeam') {
+    			break;
+    		}
+    	}
+
+    	if (isset($p)) {
+	    	$card1 = $p['hole_cards'][0];
+	    	$card2 = $p['hole_cards'][1];
+
+	    	$result = fCheckProbability($card1['rank'], $card2['rank'], $card1['suit'] == $card2['suit']);
+
+	    	return $result > 60 ? 1000000 : 0;
+	    } else {
+	    	return 1000000;
+	    }
     }
 
     public function showdown($game_state)
